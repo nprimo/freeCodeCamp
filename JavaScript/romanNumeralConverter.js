d9d2e65778d2369f3c922ec89ff02a3616da9dd9
nprimo <primo.niccolo@gmail.com>
2022-02-23 00:20:44 +0000

    Update getRomanQuantity function
    Now return 9 for specific cases

--- JavaScript/romanNumeralConverter.js
+++ JavaScript/romanNumeralConverter.js
@@ -6,7 +6,7 @@
 //   By: nprimo <marvin@42.fr>                      +#+  +:+       +#+        //
 //                                                +#+#+#+#+#+   +#+           //
 //   Created: 2022/02/22 23:14:55 by nprimo            #+#    #+#             //
-//   Updated: 2022/02/23 00:05:57 by nprimo           ###   ########.fr       //
+//   Updated: 2022/02/23 00:19:40 by nprimo           ###   ########.fr       //
 //                                                                            //
 // ************************************************************************** //
 
@@ -19,7 +19,12 @@ function getRomanQuantity(num)
 	for (i = romanValue.length - 1; i >= 0; i--) 
 	{
     	romanQuantity.unshift(Math.floor(num / romanValue[i]));
-    	num -= romanQuantity[0] * romanValue[i]; 
+		num -= romanQuantity[0] * romanValue[i];
+		if (romanQuantity[0] === 4 && romanQuantity[1] === 1)
+		{
+			romanQuantity[0] += 5;
+			romanQuantity[1] = 0;
+		}
 	}
 	return (romanQuantity);
 }
@@ -34,18 +39,25 @@ function convertToRoman(num)
 	romanQuantity = getRomanQuantity(num);
 	for (i = romanKey.length - 1; i >= 0; i--) 
 	{
-		if (romanQuantity[i] !== 4 && romanQuantity[i] !== 9) {
-			romanNum += romanKey[i].repeat(romanQuantity[i]);}
-		else
+		if (romanQuantity[i] === 4)
 		{
 			romanNum += romanKey[i];
 			romanNum += romanKey[i + 1];
+		}
+		else if (romanQuantity[i] === 9)
+		{
+			romanNum += romanKey[i];
+			romanNum += romanKey[i + 2];
+		}
+		else
+		{
+			romanNum += romanKey[i].repeat(romanQuantity[i]);
 		}
 	}
 	return (romanNum);
 }
 
-let testNumbers = ['10', '21', '14', '23', 99];
+let testNumbers = [1, 2, 3, 4, 5, 9, 12, 14, 45, 68, 79, 99];
 let i = 0;
 
 for (; i < testNumbers.length; i++)
@@ -53,4 +65,5 @@ for (; i < testNumbers.length; i++)
 	console.log('For number ' + 
 		testNumbers[i] + 
 		':' + convertToRoman(testNumbers[i]));
+	console.log(getRomanQuantity(testNumbers[i]));
 }
